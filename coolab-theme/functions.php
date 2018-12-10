9171b697b21e21922006ae53f07fddad951e57ef
bruno vianna <bruno@pobox.com>
2018-12-10 19:11:08 -0200

    Fixing CSS in link and other front-page issues

--- coolab-theme/functions.php
+++ coolab-theme/functions.php
@@ -1,7 +1,7 @@
 <?php
 if(function_exists('register_nav_menu')){
-	register_nav_menu('menu_coolab', 'menu coolab na cor verde'); 
-	register_nav_menu('menu_coolab_preto', 'menu coolab na cor preta'); 
+	register_nav_menu('menu_coolab', 'menu coolab transparente');
+	register_nav_menu('menu_coolab_preto', 'menu coolab na cor preta');
 }
 
 function estilizar_items_menu($classes, $item){
@@ -9,4 +9,13 @@ function estilizar_items_menu($classes, $item){
 	return $classes;
 }
 add_filter('nav_menu_css_class', 'estilizar_items_menu', 10, 2);
+
+// the idea here is to use bootstrap CSS (nav-item etc) in the wp menu.
+// couldn't find any other way
+
+function stylize_menu_anchor ($atts) {
+	$atts['class'] = "nav-item-link nav-link medium active";
+  return $atts;
+}
+add_filter( 'nav_menu_link_attributes', 'stylize_menu_anchor');
 ?>
