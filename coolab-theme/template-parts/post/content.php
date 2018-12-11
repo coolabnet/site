e60bee25004e4bbdd5ab24f8963240229b81515a
bruno vianna <bruno@pobox.com>
2018-12-11 16:20:35 -0200

    added blog posts content

---
+++ coolab-theme/template-parts/post/content.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Template part for displaying posts
+ *
+*/
+
+?>
+<div class="blog-post">
+	<h2 class="blog-post-title"><?php the_title(); ?></h2>
+	<p class="blog-post-meta"><?php the_date(); ?> , <?php the_author(); ?></p>
+
+	<?php if (is_single()): ?>
+
+		<p class="blog-post-content"<?php the_content(); ?></p>
+
+
+	<?php else : ?>
+
+  <p class="blog-post-content"<?php the_excerpt(); ?>
+		<br><?php the_shortlink ( $text="Leia aqui" ); ?></p>
+
+	<?php endif; ?>
+
+</div><!-- /.blog-post -->
