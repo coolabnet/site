bbac466f36d185183cbee273ba7cb5e99ab52a73
bruno vianna <bruno@pobox.com>
2018-12-11 16:18:04 -0200

    back to old menu (not differencing footer and principal)

--- coolab-theme/functions.php
+++ coolab-theme/functions.php
@@ -13,23 +13,9 @@ add_filter('nav_menu_css_class', 'estilizar_items_menu', 10, 2);
 // the idea here is to use bootstrap CSS (nav-item etc) in the wp menu.
 // couldn't find any other way
 
-<<<<<<< HEAD
-
-function stylize_menu_anchor ( $atts, $item, $args) {
-		// check if the item is in the primary menu
-		if( $args->menu == 'principal-home' ) {
-	  	// add the desired attributes:
-	    $atts['class'] = "nav-item-link nav-link medium active";
-		}
-  	return $atts;
-}
-add_filter( 'nav_menu_link_attributes', 'stylize_menu_anchor', 10, 3 );
-
-=======
 function stylize_menu_anchor ($atts) {
 	$atts['class'] = "nav-item-link nav-link medium active";
   return $atts;
 }
 add_filter( 'nav_menu_link_attributes', 'stylize_menu_anchor');
->>>>>>> 8aea5fe7b5d23c4c995e525cb71abf1ca4252f56
 ?>
